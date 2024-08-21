b533b0d24a3c96553572268979a3b9f464e14a84
xwace <37037519+xwace@users.noreply.github.com>
2024-08-21 16:44:33 +0800

    Update jps_bit_impl.cpp

--- jps_bit_impl.cpp
+++ jps_bit_impl.cpp
@@ -5,3 +5,13 @@ cv::Mat result = (~mat2)(r1)&(mat2)(r2);
 
 cout<<"mm: "<<(~mat2)(r1)<<" "<<(mat2)(r2)<<endl;
 cout<<"result: "<<result<<endl;
+
+/*地图从Mat转为二维枚举容器*/
+cv::Mat src_32s;
+src.convertTo(src_32s,CV_32S);
+randu(src,0,29);
+vector<vector<GridOccupyType>>vecG(src.rows,vector<GridOccupyType>(src.cols));
+for (size_t i = 0; i < src.rows; i++)
+{
+    memcpy(&vecG[i][0],src_32s.ptr<int>(i),sizeof(int)*src.cols);
+}
