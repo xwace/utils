7c841ab05c96c251a0a4b81eefe6776b78b3fb95
xwace <37037519+xwace@users.noreply.github.com>
2024-08-29 18:05:47 +0800

    Update jps_bit_impl.cpp

--- jps_bit_impl.cpp
+++ jps_bit_impl.cpp
@@ -1,7 +1,7 @@
 cv::Mat mat2 = (cv::Mat_<uchar>(1,6)<<0,255,0,0,255,0);
 cv::Rect r1{0,0,5,1};
 cv::Rect r2{1,0,5,1};
-cv::Mat result = (~mat2)(r1)&(mat2)(r2);
+cv::Mat result = (~mat2)(r1)&(mat2)(r2);//cv::Mat只有uchar的255取反才会变0
 
 cout<<"mm: "<<(~mat2)(r1)<<" "<<(mat2)(r2)<<endl;
 cout<<"result: "<<result<<endl;
