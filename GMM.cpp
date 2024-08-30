87b3e670a85ae1d7f85276a44d6c6e8e74d88700
xwace <37037519+xwace@users.noreply.github.com>
2024-08-30 12:15:35 +0800

    Update GMM.cpp

--- GMM.cpp
+++ GMM.cpp
@@ -1,3 +1,8 @@
+ /*
+     为了避免每次重新训练，可以只训练一次后通过checkTrainData(int startStep, const Mat& samples,int nclusters, int covMatType, const Mat* probs, const Mat* means,const std::vector<Mat>* covs, const Mat* weights)
+     获取到gmm模型的参数，u1,σ1,w1,u2,σ2,w2, 保存到代码中，再仿照predict2调用computeProbabilities(const Mat& sample, Mat* probs, int ptype)的方式，重写预测函数。
+ */
+
 cv::Ptr<cv::ml::EM>em_model;
 void GMM_calculate(cv::Mat points, cv::Mat sample) {
     cv::Mat labels;
